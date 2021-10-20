92a140beb8e197ba2e027cf68ac85c96ec5eb680
NATHANBASIS <59455965+NATHANBASIS@users.noreply.github.com>
2021-10-19 20:22:07 -0600

    Review list

---
+++ lib/review_list.dart
@@ -0,0 +1,10 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class ReviewList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
