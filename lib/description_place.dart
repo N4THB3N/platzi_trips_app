0fd958679bdda01eb04bfa638b6bd101cc504a57
NATHANBASIS <59455965+NATHANBASIS@users.noreply.github.com>
2021-10-19 19:45:16 -0600

    Google Fonts

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
 
 class DescriptionPlace extends StatelessWidget {
+  String namePlace;
+  int stars;
+  String descriptionPlace;
+
+  DescriptionPlace(this.namePlace, this.stars, this.descriptionPlace);
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+
+    final star_half = Container(
+      margin: EdgeInsets.only(top: 323.0, right: 3.0),
+      child: Icon(
+        Icons.star_half,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
+    final star_border = Container(
+      margin: EdgeInsets.only(top: 323.0, right: 3.0),
+      child: Icon(
+        Icons.star_border,
+        color: Color(0xFFf2C611),
+      ),
+    );
 
     final star = Container(
       margin: EdgeInsets.only(top: 323.0, right: 3.0),
@@ -18,8 +40,11 @@ class DescriptionPlace extends StatelessWidget {
         Container(
           margin: EdgeInsets.only(top: 320, left: 20.0, right: 20.0),
           child: Text(
-            'Duwili Ela',
-            style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w900),
+            namePlace,
+            style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.w900,
+                fontFamily: "Lato"),
             textAlign: TextAlign.left,
           ),
         ),
@@ -35,6 +60,24 @@ class DescriptionPlace extends StatelessWidget {
       ],
     );
 
-    return title_stars;
+    final description = Container(
+        margin: EdgeInsets.only(
+          left: 20,
+          right: 20,
+          top: 20,
+        ),
+        child: Text(
+          descriptionPlace,
+          style: TextStyle(
+              fontSize: 16,
+              fontFamily: "Lato",
+              fontWeight: FontWeight.bold,
+              color: Color(0xFF56575a)),
+          textAlign: TextAlign.justify,
+        ));
+
+    return Column(
+      children: [title_stars, description],
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ class MyApp extends StatelessWidget {
           title: Text("Hola Mundo Feliz"),
         ),
         // body: Text(''),
-        body: DescriptionPlace(),
+        body: DescriptionPlace('Duwili Ela', 4,
+            'Lorem ipsum dolor sit amet, consecteuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pelientesque eu. Pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdient a.'),
       ),
     );
   }
