10614816754f8208703f4ba9efde5908719c58fb
NATHANBASIS <59455965+NATHANBASIS@users.noreply.github.com>
2021-10-24 15:49:16 -0600

    All content done.

---
+++ lib/home_trips.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'description_place.dart';
+import 'review_list.dart';
+import 'header_appbar.dart';
+
+class HomeTrips extends StatelessWidget {
+  String descriptionDummy =
+      'Lorem ipsum dolor sit amet, consecteuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pelientesque eu. Pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdient a.';
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Stack(
+      children: <Widget>[
+        ListView(
+          children: <Widget>[
+            DescriptionPlace('Duwili Ela', 4, descriptionDummy),
+            ReviewList(),
+            // DesignerList()
+          ],
+        ),
+        // GradientBack("Popular"),
+        HeaderAppBar(),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,11 @@
 import 'package:flutter/material.dart';
-import 'description_place.dart';
-import 'review_list.dart';
-import 'gradient_back.dart';
-import 'header_appbar.dart';
-import 'designer_list.dart';
+import 'platzi_trips.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  String descriptionDummy =
-      'Lorem ipsum dolor sit amet, consecteuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pelientesque eu. Pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdient a.';
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hola Mundo Feliz"),
-        ),
-        // body: Text(''),
-        // body: DescriptionPlace('Duwili Ela', 4,
-        //     'Lorem ipsum dolor sit amet, consecteuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pelientesque eu. Pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdient a.'),
-        // body: ReviewList(),
-        body: Stack(
-          children: <Widget>[
-            ListView(
-              children: <Widget>[
-                DescriptionPlace('Duwili Ela', 4, descriptionDummy),
-                ReviewList(),
-                // DesignerList()
-              ],
-            ),
-            // GradientBack("Popular"),
-            HeaderAppBar(),
-          ],
-        ),
-      ),
-    );
+    return MaterialApp(title: 'Flutter Demo', home: PlatziTrips());
   }
 }
